cf06cae4c17374ac56763c38cbe9555adcb7bcc2
Torjus Åmellem Kallekleiv <torjus.kallekleiv@gmail.com>
2025-09-13 10:38:21 +0200

    Fix Swift LSP

--- init.lua
+++ init.lua
@@ -783,12 +783,13 @@ require('lazy').setup({
   require 'plugins.neo-tree',
   require 'plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'plugins.neo-git',
+  require 'plugins.lsp-config', -- Custom LSP configurations
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  { import = 'custom.plugins' },
+  { import = 'plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
