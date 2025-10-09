9e65c1e6da23b0ecaf58bc1c9d16342b2be8bbd3
Torjus Åmellem Kallekleiv <torjus.kallekleiv@gmail.com>
2025-10-09 12:53:17 +0200

    Add some keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -40,3 +40,32 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+
+-- [[ Debugger Keymaps ]]
+-- Nvim DAP
+local map = vim.keymap.set
+map("n", "<Leader>dl", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Debugger step into" })
+map("n", "<Leader>dj", "<cmd>lua require'dap'.step_over()<CR>", { desc = "Debugger step over" })
+map("n", "<Leader>dk", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Debugger step out" })
+map("n", "<Leader>dc", "<cmd>lua require'dap'.continue()<CR>", { desc = "Debugger continue" })
+map("n", "<Leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "Debugger toggle breakpoint" })
+map(
+	"n",
+	"<Leader>dd",
+	"<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+	{ desc = "Debugger set conditional breakpoint" }
+)
+map("n", "<Leader>de", "<cmd>lua require'dap'.terminate()<CR>", { desc = "Debugger reset" })
+map("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugger run last" })
+
+-- rustaceanvim
+map("n", "<Leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = "Debugger testables" })
+
+-- [[ Rust Compiler Keymaps ]]
+-- Cargo commands
+map("n", "<Leader>cc", "<cmd>!cargo check<CR>", { desc = "Cargo check" })
+map("n", "<Leader>cb", "<cmd>!cargo build<CR>", { desc = "Cargo build" })
+map("n", "<Leader>cr", "<cmd>!cargo run<CR>", { desc = "Cargo run" })
+map("n", "<Leader>ct", "<cmd>!cargo test<CR>", { desc = "Cargo test" })
+map("n", "<Leader>cf", "<cmd>!cargo fmt<CR>", { desc = "Cargo format" })
+map("n", "<Leader>cl", "<cmd>!cargo clippy<CR>", { desc = "Cargo clippy" })
